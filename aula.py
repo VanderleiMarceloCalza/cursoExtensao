04e16f4e8895dcf5cf544cbe3a6654775f814503
Vanderlei Calza <Vandermcalza@outlook.com>
2025-02-13 16:10:59 -0300

    adicionando observaçoes

--- aula.py
+++ aula.py
@@ -1,6 +1,15 @@
-print('Olá, mundo!')
-print('Alterando para teste')
-print('Segunda alteração')
-print('fazendo alteração para push')
-print('Olá, mundo!')
-print('Olá, mundo!')
+print('Olá, mundo!' + 'create readme')
+print('Alterando para teste' + 'adicionando o py')
+print('Segunda alteração' + 'alterando aula py')
+print('fazendo alteração para push' + 'segunda alteraçaõ py')
+print('Olá, mundo!' + 'fazendo push')
+print('Olá, mundo!' + 'arrumando tudo')
+print('depois de mudar algo
+fazer o git add .
+fazer o git commit -m "comentario"
+pode ser feito o git diff para ver a diferença
+ai entao pode fazer o git push origin main
+se precisar voltar atras 
+usa-se o git log --oneline para ver onde predende voltar, copia o codigo
+digita git restore --source cola o codigo
+na sequencia add commit push')
